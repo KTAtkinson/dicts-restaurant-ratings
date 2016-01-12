ec20cf1f854e968dccd52eec255aa381ed1fac59
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 16:12:54 -0800

    defined alphabetical ratings function

---
+++ restaurant-ratings.py
@@ -0,0 +1,17 @@
+# your code goes here
+import sys
+
+def print_alphabetical_ratings(filepath):
+    ratings_file = open(filepath)
+    restaurant_ratings = {}
+
+    for line in ratings_file:
+        restaurant, rating = line.rstrip().split(':')
+        restaurant_ratings[restaurant] = rating
+
+    sorted_restaurants = sorted(restaurant_ratings.keys())
+
+    for restaurant in sorted_restaurants:
+        print "{} is rated at {}.".format(restaurant, restaurant_ratings[restaurant])
+
+print_alphabetical_ratings(sys.argv[1])
